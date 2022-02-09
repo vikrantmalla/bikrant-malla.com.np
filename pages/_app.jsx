bbb5010b680d7aa0e0f11d70c1144b07fcc0cd5f
Vikrant Malla <vikrantmalla999@gmail.com>
2022-02-09 21:05:20 +0545

    add nProgess

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,6 +5,13 @@ import MouseContext from '../helpers/context/MouseContext'
 import DotRing from '../components/cursor/DotRing'
 import Footer from '../components/footer/Footer'
 import ScrollArrow from '../components/scrollup/ScrollArrow'
+import Router from "next/router"
+import nProgress from "nprogress"
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
+
 function MyApp({ Component, pageProps }) {
     return (
         <ThemeContext>
