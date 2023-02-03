84fc2e29115f75189a1a36b52b4d429e3a9d3fb4
Bikrant Malla <bikrant.malla@truenary.com>
2023-02-03 22:30:36 +0545

    feat: add page loader

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,15 @@ class LinkDocument extends Document {
             integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
             crossOrigin="anonymous"
           />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;500;600&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Teko:wght@700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
--- pages/archive.tsx
+++ pages/archive.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import { GetServerSideProps } from "next";
 import ArchiveHeader from "../components/archive/ArchiveHeader";
 import baseUrl from "../helpers/lib/baseUrl";
@@ -8,71 +7,6 @@ import Data, { PageData } from "../types/data";
 const Archive = ({ projectData }: PageData.ArchivePageData) => {
   return (
     <>
-      <Head>
-        {/* <title>Archive - Bikrant Malla</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="Bikrant Malla - Creative frontEnd web developer who loves javascript and modern web technologies."
-        />
-        <meta
-          name="keywords"
-          content="bikrant malla, vikrant, ui/ux, portfolio, web, frontend, developer, frontend enginner, project"
-        />
-        <meta name="author" content="Bikrant Malla" />
-        <meta
-          name="google-site-verification"
-          content="pjAbOBn26n0Q6sNbPKdsSIb4F921MU1XrRja9irTkXU"
-        />
-        <meta
-          name="robots"
-          content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="Bikrant Malla" />
-        <meta
-          property="og:title"
-          content="Bikrant Malla - Frontend Developer"
-        />
-        <meta
-          property="og:description"
-          content="Bikrant Malla - Creative frontEnd web developer who loves javascript and modern web technologies."
-        />
-        <meta
-          property="og:url"
-          content="https://www.bikrant-malla.com.np/archive"
-        />
-        <meta property="fb:app_id" content="668583534114468" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@Vikrantmalla09" />
-        <meta name="twitter:creator" content="@Vikrantmalla09" />
-        <meta
-          property="og:url"
-          content="https://www.bikrant-malla.com.np/archive"
-        />
-        <meta
-          property="og:title"
-          content="Bikrant Malla - Frontend Developer"
-        />
-        <meta
-          property="og:description"
-          content="Bikrant Malla - Creative frontEnd web developer who loves javascript and modern web technologies."
-        />
-        <meta
-          property="og:image"
-          content="https://www.bikrant-malla.com.np/ogimg.png"
-        />
-        <meta
-          name="google-site-verification"
-          content="pjAbOBn26n0Q6sNbPKdsSIb4F921MU1XrRja9irTkXU"
-        /> */}
-        <link
-          rel="stylesheet"
-          href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
-          integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
-          crossOrigin="anonymous"
-        />
-      </Head>
       <section className="container">
         <ArchiveHeader />
         <div className="archive">
