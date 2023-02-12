2c9ca2519fc07dc8e27f8ecd00bc5621d187223d
Bikrant Malla <bikrant.malla@truenary.com>
2023-02-12 17:12:02 +0545

    test: api testing

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,9 @@
 // Used for __tests__/testing-library.js
 import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom';
+import { configure } from '@testing-library/react';
+import { TextDecoder, TextEncoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+configure({ asyncUtilTimeout: 400 });
--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  testEnvironment: 'node'
 }
 
 module.exports = nextConfig
