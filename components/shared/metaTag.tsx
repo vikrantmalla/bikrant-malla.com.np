d6fc57518155ef5987aa7a544e412b0dc68f9506
Bikrant Malla <vikrantmalla999@gamil.com>
2023-08-13 22:09:11 +0545

    feat: use getInitialProps to fetch metatags

--- components/shared/metaTag.tsx
+++ components/shared/metaTag.tsx
@@ -1,70 +1,67 @@
+import { MetaTagData } from "@/types/data";
 import Head from "next/head";
 import { useRouter } from "next/router";
 
-const MetaTags = () => {
+const MetaTags = ({ metaTagData }: any) => {
   const router = useRouter();
   return (
     <Head>
       {router.pathname === "/" ? (
-        <title> Bikrant Malla - Frontend Web Developer </title>
+        <title> {metaTagData.metaTag[0].title} </title>
       ) : (
-        <title>Archive - Bikrant Malla</title>
+        <title>{metaTagData.metaTag[0].pageTitle}</title>
       )}
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta
         name="description"
-        content="Bikrant Malla - Experienced Front-End Web Developer specializing in JavaScript and cutting-edge web technologies, delivering creative solutions."
+        content={metaTagData.metaTag[0].description}
       />
       <meta
         name="keywords"
-        content="bikrant malla, vikrant malla, ui/ux, portfolio, web, web developer, frontend, developer, frontend enginner, project, design"
+        content={metaTagData.metaTag[0].keywords}
       />
-      <meta name="author" content="Bikrant Malla" />
+      <meta name="author" content={metaTagData.metaTag[0].author} />
       <meta
         name="google-site-verification"
-        content="pjAbOBn26n0Q6sNbPKdsSIb4F921MU1XrRja9irTkXU"
+        content={metaTagData.metaTag[0].googleSiteID}
       />
       <meta
         name="robots"
         content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"
       />
       <meta property="og:type" content="website" />
-      <meta property="og:site_name" content="Bikrant Malla" />
+      <meta property="og:site_name" content={metaTagData.metaTag[0].author} />
       {router.pathname === "/" ? (
         <meta
           property="og:title"
-          content="Bikrant Malla - Frontend Web Developer"
+          content={metaTagData.metaTag[0].title}
         />
       ) : (
-        <meta property="og:title" content="Archive - Bikrant Malla" />
+        <meta property="og:title" content={metaTagData.metaTag[0].pageTitle} />
       )}
       <meta
         property="og:description"
-        content="Bikrant Malla - Experienced Front-End Web Developer specializing in JavaScript and cutting-edge web technologies, delivering creative solutions."
+        content={metaTagData.metaTag[0].description}
       />
       {router.pathname === "/" ? (
-        <meta property="og:url" content="https://www.bikrant-malla.com.np" />
+        <meta property="og:url" content={`${process.env.NEXT_PUBLIC_APP_URL}`} />
       ) : (
         <meta
           property="og:url"
-          content="https://www.bikrant-malla.com.np/archive"
+          content={`${process.env.NEXT_PUBLIC_APP_URL}/archive`}
         />
       )}
-      <meta property="fb:app_id" content="668583534114468" />
+      <meta property="fb:app_id" content={metaTagData.metaTag[0].fbID} />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@Vikrantmalla09" />
-      <meta name="twitter:creator" content="@Vikrantmalla09" />
+      <meta name="twitter:site" content={metaTagData.metaTag[0].twitterID} />
+      <meta name="twitter:creator" content={metaTagData.metaTag[0].twitterID} />
       <meta
         property="og:description"
-        content="Bikrant Malla - Experienced Front-End Web Developer specializing in JavaScript and cutting-edge web technologies, delivering creative solutions."
+        content={metaTagData.metaTag[0].description}
       />
       <meta
         property="og:image"
-        content="https://www.bikrant-malla.com.np/ogimg.png"
-      />
-      <meta
-        name="google-site-verification"
-        content="pjAbOBn26n0Q6sNbPKdsSIb4F921MU1XrRja9irTkXU"
+        content={`${process.env.NEXT_PUBLIC_APP_URL}/ogimg.png`}
       />
     </Head>
   );
