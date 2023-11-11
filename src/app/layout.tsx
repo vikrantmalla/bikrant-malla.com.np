74b2032a90b328bab9ce3f67ce61eba431ea29bf
Bikrant Malla <vikrantmalla999@gamil.com>
2023-11-11 18:04:13 +0545

    feat: remove a react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import ThemeProvider from "@/context/ThemeContext";
-import Provider from "@/utils/provider";
 import NavBar from "@/components/shared/header/NavBar";
 import Footer from "@/components/shared/footer/Footer";
 import "../styles/globals.scss";
@@ -18,11 +17,9 @@ export default function RootLayout({
     <html lang="en">
       <ThemeProvider>
         <body>
-          <Provider>
             <NavBar />
             {children}
             <Footer />
-          </Provider>
         </body>
       </ThemeProvider>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import AboutMe from '@/components/intro/AboutMe'
+import { fetchAboutMeData } from '@/service/apiService';
 
-const Home = () => {
+const Home = async () => {
+  const aboutMeData = await fetchAboutMeData();
   return (
     <main>
-        <article className="container">
-          <AboutMe />
-        </article>
-      </main>
+      <article className="container">
+        <AboutMe aboutMeData={aboutMeData} />
+      </article>
+    </main>
   )
 }
 
