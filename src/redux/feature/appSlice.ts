5655e8ba1972e623baff52413d1af14498791ebb
Vikrant Malla <vikrantmalla999@gmail.com>
2024-01-06 20:58:40 +0545

    feat: add a appSlice

---
+++ src/redux/feature/appSlice.ts
@@ -0,0 +1,27 @@
+import { ProviderContext } from "@/types/data";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState: ProviderContext.AppSlice = {
+  toggleMenu: false,
+  navColor: false,
+  activeLink: "About Me"
+};
+
+export const app = createSlice({
+  name: "app",
+  initialState,
+  reducers: {
+    setToggleMenu: (state, action) => {
+      state.toggleMenu = action.payload;
+    },
+    setNavColor: (state, action) => {
+      state.navColor = action.payload;
+    },
+    setActiveLink: (state, action) => {
+      state.navColor = action.payload;
+    },
+  },
+});
+
+export const { setToggleMenu, setNavColor, setActiveLink } = app.actions;
+export default app.reducer;
