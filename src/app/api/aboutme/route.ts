73c94a7567b90e144bc366208a92790f2c1c412d
Vikrant Malla <vikrantmalla999@gmail.com>
2024-02-02 23:33:17 +0545

    feat: add POST method in aboutme

--- src/app/api/aboutme/route.ts
+++ src/app/api/aboutme/route.ts
@@ -1,6 +1,6 @@
 import dbConnect from "../../../helpers/lib/dbConnect";
 import Aboutme from "../../../helpers/models/Aboutme";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 dbConnect();
 export async function GET() {
@@ -9,6 +9,30 @@ export async function GET() {
     return NextResponse.json(
       { success: true, aboutme: aboutmeDetails },
       { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Internal server error",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const aboutme = await new Aboutme(body);
+    await aboutme.save();
+    return NextResponse.json(
+      {
+        success: true,
+        aboutme,
+        message: "Data saved successfully",
+      },
+      { status: 201 }
     );
   } catch (error) {
     return NextResponse.json(
