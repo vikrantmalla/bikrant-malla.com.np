97c1258830333fce80dcab07c2088959b8e39cfd
Vikrant Malla <vikrantmalla999@gmail.com>
2024-02-10 22:01:16 +0545

    feat: add GET POST PUT DELETE method in project route

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,19 +1,116 @@
 import dbConnect from "@/helpers/lib/dbConnect";
 import Project from "@/helpers/models/Project";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 dbConnect();
+
 export async function GET() {
   try {
     const projects = await Project.find().sort({ isnew: -1 });
-    return NextResponse.json({
-      success: true,
-      projectHighlights: projects,
-    });
+    return NextResponse.json(
+      { success: true,  project: projects },
+      { status: 200 }
+    );
   } catch (error) {
-    return NextResponse.json({
-      success: false,
-      message: "Internal server error",
-    });
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Internal server error",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const project = await new Project(body);
+    await project.save();
+    return NextResponse.json(
+      {
+        success: true,
+        project,
+        message: "Data saved successfully",
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Internal server error",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const { _id, ...updatedData } = body;
+
+    const updatedProjectData = await Project.findByIdAndUpdate(
+      _id,
+      updatedData,
+      { new: true }
+    );
+
+    if (updatedProjectData) {
+      return NextResponse.json(
+        {
+          success: true,
+          behanceProject: updatedProjectData,
+          message: "Data updated successfully",
+        },
+        { status: 200 }
+      );
+    } else {
+      return NextResponse.json(
+        { success: false, message: "Data not found" },
+        { status: 404 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Internal server error",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+
+export async function DELETE(request: NextRequest) {
+  try {
+    const { _id } = await request.json();
+
+    const deletedProjectData = await Project.findByIdAndDelete(_id);
+
+    if (deletedProjectData) {
+      return NextResponse.json(
+        {
+          success: true,
+          message: "Data deleted successfully",
+        },
+        { status: 200 }
+      );
+    } else {
+      return NextResponse.json(
+        { success: false, message: "Data not found" },
+        { status: 404 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Internal server error",
+      },
+      { status: 500 }
+    );
   }
 }
