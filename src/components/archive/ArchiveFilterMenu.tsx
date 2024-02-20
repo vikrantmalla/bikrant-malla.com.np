ea0be4594d4d9f3c6ec1ecb5acab3ad2cfb78794
Vikrant Malla <vikrantmalla999@gmail.com>
2024-02-20 22:19:56 +0545

    fix: filter tag style

--- src/components/archive/ArchiveFilterMenu.tsx
+++ src/components/archive/ArchiveFilterMenu.tsx
@@ -1,22 +1,32 @@
 "use client";
-import { joseFont } from "@/helpers/lib/font";
-import Data, { ArchiveList } from "../../types/data";
-import { useSearchParams } from "next/navigation";
-import Link from "next/link";
 import { useCallback, useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/redux/store";
-import {  setProjectList, setSelectedTag } from "@/redux/feature/projectSlice";
+import Link from "next/link";
+import { joseFont } from "@/helpers/lib/font";
+import Data, { ArchiveList } from "../../types/data";
+import { setProjectList, setSelectedTag } from "@/redux/feature/projectSlice";
 
 const ArchiveFilterMenu = ({ project }: ArchiveList) => {
   const dispatch = useDispatch<AppDispatch>();
-  const selectedTag = useSelector((state: RootState) => state.project.selectedTag); 
+  const selectedTag = useSelector(
+    (state: RootState) => state.project.selectedTag
+  );
   const search = useSearchParams();
 
-  const filterProjects = useCallback((tag: string) => {
-    const filteredProjects = tag === "All" ? project : project.filter((p: Data.ArchiveDetailsData) => p.build.includes(tag));
-    dispatch(setProjectList(filteredProjects));
-  }, [project, dispatch]);
+  const filterProjects = useCallback(
+    (tag: string) => {
+      const filteredProjects =
+        tag === "All"
+          ? project
+          : project.filter((p: Data.ArchiveDetailsData) =>
+              p.build.includes(tag)
+            );
+      dispatch(setProjectList(filteredProjects));
+    },
+    [project, dispatch]
+  );
 
   useEffect(() => {
     const initialTagFromUrl = search.get("tag") || "All";
@@ -32,12 +42,20 @@ const ArchiveFilterMenu = ({ project }: ArchiveList) => {
   const tech = ["All", "Feature", "HTML", "CSS", "React JS"];
 
   return (
-    <div className={`subheading ${joseFont} fs-600`}>
-      {tech.map((techTag) => (
-        <Link key={techTag} href={`?tag=${techTag}`} onClick={() => handleTagSelect(techTag)}>
-          {techTag}
-        </Link>
-      ))}
+    <div className={`subheading ${joseFont} fs-300 filter-links`}>
+      <ul>
+        {tech.map((techTag) => (
+          <li key={techTag}>
+            <Link
+              href={`?tag=${techTag}`}
+              onClick={() => handleTagSelect(techTag)}
+              className="tags"
+            >
+              {techTag}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
