b48811e98675897cc5725071987583a14795b48e
Vikrant Malla <vikrantmalla999@gmail.com>
2024-02-24 00:20:34 +0545

    feat: add a robots and sitemap pages

---
+++ src/app/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_APP_URL}`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_APP_URL}/archive`,
+      lastModified: new Date(),
+    },
+  ];
+}
