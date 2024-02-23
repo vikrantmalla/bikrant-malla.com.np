56a8820758bb660d6c9d9cca8afdaff9f4ba439e
Vikrant Malla <vikrantmalla999@gmail.com>
2024-02-24 00:34:44 +0545

    chore: update a clarity script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -94,7 +94,7 @@ export default async function RootLayout({
         {`
           (function(c,l,a,r,i,t,y){
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
           })(window, document, "clarity", "script", "l6dex549t7");
         `}
