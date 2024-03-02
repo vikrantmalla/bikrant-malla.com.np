867f2c670150a513ec94f04c4719b5be80fd5a76
Vikrant Malla <vikrantmalla999@gmail.com>
2024-03-02 21:42:41 +0545

    feat: add a project is not available text when project is empty

--- src/components/archive/ArchiveList.tsx
+++ src/components/archive/ArchiveList.tsx
@@ -2,16 +2,27 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
+import { Project } from "../../types/data";
+import { joseFont } from "@/helpers/lib/font";
 import ArchiveDetails from "./ArchiveCard";
-import Data, { Project } from "../../types/data";
 
 const Card = () => {
-  const projectList = useSelector((state: RootState) => state.project.projectList);
+  const projectList = useSelector(
+    (state: RootState) => state.project.projectList
+  );
   return (
     <>
-      {...projectList.map((data: Project, id: number) => {
-        return <ArchiveDetails key={id} {...data} isnew={data.isnew} />;
-      })}
+      {projectList.length > 0 ? (
+        projectList.map((data: Project, id: number) => (
+          <ArchiveDetails key={id} {...data} isnew={data.isnew} />
+        ))
+      ) : (
+        <div className="message-placeholder">
+          <p className={`subheading ${joseFont} fs-600`}>
+            Project is not available
+          </p>
+        </div>
+      )}
     </>
   );
 };
