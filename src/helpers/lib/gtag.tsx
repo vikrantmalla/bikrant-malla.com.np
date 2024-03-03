0cc17e339b50177c9c631b0a9d85e1460b946c8e
Vikrant Malla <vikrantmalla999@gmail.com>
2024-03-03 18:39:29 +0545

    chore: remove console.log

--- src/helpers/lib/gtag.tsx
+++ src/helpers/lib/gtag.tsx
@@ -19,7 +19,6 @@ export const pageview = (url: URL) => {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label }: GTagEvent) => {
-  console.log(action)
   if (typeof window.gtag !== "undefined") {
     window.gtag("event", action, {
       event_category: category,
