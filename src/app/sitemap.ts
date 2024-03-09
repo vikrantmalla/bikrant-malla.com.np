0da121c079df4be487bf3f6bd3a975eaf79eea01
Vikrant Malla <vikrantmalla999@gmail.com>
2024-03-09 11:24:13 +0545

    feat: update a sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -14,5 +14,29 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: `${process.env.NEXT_PUBLIC_APP_URL}/resume.pdf`,
       lastModified: new Date(),
     },
+    {
+      url: `${process.env.NEXT_PUBLIC_APP_URL}/archive?tag=all`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_APP_URL}/archive?tag=feature`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_APP_URL}/archive?tag=html`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_APP_URL}/archive?tag=css`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_APP_URL}/archive?tag=scss`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_APP_URL}/archive?tag=react_js`,
+      lastModified: new Date(),
+    },
   ];
 }
