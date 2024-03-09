682923ff554806accd22a004ae74647ab04fb575
Vikrant Malla <vikrantmalla999@gmail.com>
2024-03-09 10:13:25 +0545

    feat: add a api response return type

--- src/service/apiService.tsx
+++ src/service/apiService.tsx
@@ -1,44 +1,46 @@
 import baseUrl from "@/helpers/lib/baseUrl";
+import Data from "@/types/data";
 
-export async function fetchAboutMeData() {
-  const res = await fetch(`${baseUrl}/api/aboutme`, { cache: 'no-store' });
-  const data = await res.json();
+export async function fetchAboutMeData(): Promise<Data.AboutMeData> {
+  const res = await fetch(`${baseUrl}/api/aboutme`, { cache: "no-store" });
+  const data: Data.AboutMeData = await res.json();
   return data;
 }
 
-
-export async function fetchProjectHighlights() {
-  const res = await fetch(`${baseUrl}/api/projecthighlight`, { cache: 'no-store' });
-  const data = await res.json();
+export async function fetchProjectHighlights(): Promise<Data.ProjectHighlightData> {
+  const res = await fetch(`${baseUrl}/api/projecthighlight`, {
+    cache: "no-store",
+  });
+  const data: Data.ProjectHighlightData = await res.json();
   return data;
 }
 
-export async function fetchBehanceData() {
-  const res = await fetch(`${baseUrl}/api/behance`, { cache: 'no-store' });
-  const data = await res.json();
+export async function fetchBehanceData(): Promise<Data.BehanceData> {
+  const res = await fetch(`${baseUrl}/api/behance`, { cache: "no-store" });
+  const data: Data.BehanceData = await res.json();
   return data;
 }
 
-export async function fetchProjectData() {
-  const res = await fetch(`${baseUrl}/api/projects`, { cache: 'no-store' });
-  const data = await res.json();
+export async function fetchProjectData(): Promise<Data.ProjectData> {
+  const res = await fetch(`${baseUrl}/api/projects`, { cache: "no-store" });
+  const data: Data.ProjectData = await res.json();
   return data;
 }
 
-export async function fetchContactData() {
-  const res = await fetch(`${baseUrl}/api/contact`, { cache: 'no-store' });
-  const data = await res.json();
+export async function fetchContactData(): Promise<Data.ContactData> {
+  const res = await fetch(`${baseUrl}/api/contact`, { cache: "no-store" });
+  const data: Data.ContactData = await res.json();
   return data;
 }
 
-export async function fetchMetaData() {
-  const res = await fetch(`${baseUrl}/api/metadata`, { cache: 'no-store' });
-  const data = await res.json();
+export async function fetchMetaData(): Promise<Data.MetaTagData> {
+  const res = await fetch(`${baseUrl}/api/metadata`, { cache: "no-store" });
+  const data: Data.MetaTagData = await res.json();
   return data;
 }
 
-// export async function fetchTagData() {
-//   const res = await fetch(`${baseUrl}/api/tags`, { cache: 'no-store' });
+// export async function fetchTagData()  {
+//  const res = await fetch(`${baseUrl}/api/tags`, { cache: "no-store" });
 //   const data = await res.json();
 //   return data;
-// }
+// // }
