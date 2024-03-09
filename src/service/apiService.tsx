2cdfb361509be5ade6e1fa43540be5cd3cdd8751
Vikrant Malla <vikrantmalla999@gmail.com>
2024-03-09 12:50:48 +0545

    feat: add a API endpoints

--- src/service/apiService.tsx
+++ src/service/apiService.tsx
@@ -1,46 +1,36 @@
 import baseUrl from "@/helpers/lib/baseUrl";
 import Data from "@/types/data";
+import { ApiEndpoint } from "@/types/enum";
 
-export async function fetchAboutMeData(): Promise<Data.AboutMeData> {
-  const res = await fetch(`${baseUrl}/api/aboutme`, { cache: "no-store" });
-  const data: Data.AboutMeData = await res.json();
+async function fetchData<T>(endpoint: string): Promise<T> {
+  const res = await fetch(`${baseUrl}/${endpoint}`, { cache: "no-store" });
+  const data: T = await res.json();
   return data;
 }
 
+export async function fetchAboutMeData(): Promise<Data.AboutMeData> {
+  return fetchData<Data.AboutMeData>(ApiEndpoint.AboutMe);
+}
 export async function fetchProjectHighlights(): Promise<Data.ProjectHighlightData> {
-  const res = await fetch(`${baseUrl}/api/projecthighlight`, {
-    cache: "no-store",
-  });
-  const data: Data.ProjectHighlightData = await res.json();
-  return data;
+  return fetchData<Data.ProjectHighlightData>(ApiEndpoint.ProjectHighlights);
 }
 
 export async function fetchBehanceData(): Promise<Data.BehanceData> {
-  const res = await fetch(`${baseUrl}/api/behance`, { cache: "no-store" });
-  const data: Data.BehanceData = await res.json();
-  return data;
+  return fetchData<Data.BehanceData>(ApiEndpoint.Behance);
 }
 
 export async function fetchProjectData(): Promise<Data.ProjectData> {
-  const res = await fetch(`${baseUrl}/api/projects`, { cache: "no-store" });
-  const data: Data.ProjectData = await res.json();
-  return data;
+  return fetchData<Data.ProjectData>(ApiEndpoint.Projects);
 }
 
 export async function fetchContactData(): Promise<Data.ContactData> {
-  const res = await fetch(`${baseUrl}/api/contact`, { cache: "no-store" });
-  const data: Data.ContactData = await res.json();
-  return data;
+  return fetchData<Data.ContactData>(ApiEndpoint.Contact);
 }
 
 export async function fetchMetaData(): Promise<Data.MetaTagData> {
-  const res = await fetch(`${baseUrl}/api/metadata`, { cache: "no-store" });
-  const data: Data.MetaTagData = await res.json();
-  return data;
+  return fetchData<Data.MetaTagData>(ApiEndpoint.MetaData);
 }
 
 // export async function fetchTagData()  {
-//  const res = await fetch(`${baseUrl}/api/tags`, { cache: "no-store" });
-//   const data = await res.json();
-//   return data;
+// fetchData(ApisEndpoint.Tags)
 // // }
