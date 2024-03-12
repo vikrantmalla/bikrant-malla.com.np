d2eae736c7ae00feecdff4cc27dcab8bdc749a1a
Vikrant Malla <vikrantmalla999@gmail.com>
2024-03-12 21:39:39 +0545

    feat: add a portfolioDetails as a props in Aboutme component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import AboutMe from '@/components/intro/AboutMe'
-import Behance from '@/components/behance/Behance';
-import Contact from '@/components/shared/footer/Contact';
-import ProjectHighlight from '@/components/project/ProjectHighlight';
-import { fetchAboutMeData, fetchBehanceData, fetchContactData, fetchProjectHighlights } from '@/service/apiService';
+import React from "react";
+import AboutMe from "@/components/intro/AboutMe";
+import Behance from "@/components/behance/Behance";
+import Contact from "@/components/shared/footer/Contact";
+import ProjectHighlight from "@/components/project/ProjectHighlight";
+import {
+  fetchPortfolioDetailsData
+} from "@/service/apiService";
 
 const Home = async () => {
-  const aboutMeData = await fetchAboutMeData();
-  const projectHighlightData = await fetchProjectHighlights();
-  const behanceData = await fetchBehanceData();
-  const contactData = await fetchContactData();
+  const portfolioDetails = await fetchPortfolioDetailsData();
+  const { aboutme } = portfolioDetails;
   return (
     <main>
       <article className="container">
-        <AboutMe aboutMeData={aboutMeData} />
-        <ProjectHighlight projectHighlightData={projectHighlightData} />
+        <AboutMe aboutme={aboutme} />
+        {/* <ProjectHighlight projectHighlightData={projectHighlightData} />
         <Behance behanceData={behanceData} />
-        <Contact contactData={contactData} />
+        <Contact contactData={contactData} /> */}
       </article>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
