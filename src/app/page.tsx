959cba56d60b673d17403c507291f556d1c5476e
Vikrant Malla <vikrantmalla999@gmail.com>
2024-03-15 23:27:39 +0545

    feat: add a contact details as props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,18 @@ import AboutMe from "@/components/intro/AboutMe";
 import Behance from "@/components/behance/Behance";
 import Contact from "@/components/shared/footer/Contact";
 import ProjectHighlight from "@/components/project/ProjectHighlight";
-import {
-  fetchPortfolioDetailsData
-} from "@/service/apiService";
+import { fetchPortfolioDetailsData } from "@/service/apiService";
 
 const Home = async () => {
   const portfolioDetails = await fetchPortfolioDetailsData();
-  const { aboutme, project, behance } = portfolioDetails;
+  const { aboutme, project, behance, contact } = portfolioDetails;
   return (
     <main>
       <article className="container">
         <AboutMe aboutme={aboutme} />
         <ProjectHighlight project={project} />
         <Behance behance={behance} />
-        {/* <Contact contactData={contactData} /> */}
+        <Contact contact={contact} />
       </article>
     </main>
   );
