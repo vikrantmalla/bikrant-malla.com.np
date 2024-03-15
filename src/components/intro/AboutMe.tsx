dbcd23fe46758357a753748f7b017114e61aad29
Vikrant Malla <vikrantmalla999@gmail.com>
2024-03-15 21:14:25 +0545

    fix: aboutme data types

--- src/components/intro/AboutMe.tsx
+++ src/components/intro/AboutMe.tsx
@@ -1,18 +1,13 @@
 "use client";
 import React from "react";
 import { joseFont, tekoFont } from "@/helpers/lib/font";
-import { AboutMeData } from "@/types/data";
+import { AboutMe, AboutMeData  } from "@/types/data";
 
-interface Props {
-  tech: string;
-  index: number;
-}
-
-const AboutMe = ({ aboutme }: AboutMeData) => {
+const AboutMe = ({ aboutme }: AboutMeData ) => {
   return (
     <>
       <section>
-        {aboutme.map((intro, index) => (
+        {aboutme.map((intro: AboutMe, index: number) => (
           <div className="intro" key={index}>
             <h1 className={`name ${joseFont} fs-600`}>
               {intro.subTitle}
@@ -27,7 +22,7 @@ const AboutMe = ({ aboutme }: AboutMeData) => {
       </section>
       <section id="aboutme">
         <h2 className={`headingleft ${joseFont} fs-600`}>About me</h2>
-        {aboutme.map((about, index) => {
+        {aboutme.map((about: AboutMe, index: number) => {
           const { aboutDescription1, aboutDescription2 } = about;
           return (
             <div className="aboutme" key={index}>
@@ -40,7 +35,7 @@ const AboutMe = ({ aboutme }: AboutMeData) => {
       <section id="skill">
         <h2 className={`headingright ${joseFont} fs-600`}>Skillsets</h2>
         <div>
-          {aboutme.map((skill, index) => {
+          {aboutme.map((skill: AboutMe, index: number) => {
             const { skill1, skill2 } = skill;
             return (
               <div className="techstack" key={index}>
@@ -48,7 +43,7 @@ const AboutMe = ({ aboutme }: AboutMeData) => {
                   <h2 className={`${joseFont} fs-500`}>
                     {`I'm`} <span>Comfortable</span> With
                   </h2>
-                  {skill1.map(({ tech, index }: Props) => {
+                  {skill1.map((tech: string, index: number) => {
                     return (
                       <span className={`${joseFont} fs-400`} key={index}>
                         {tech}
@@ -60,7 +55,7 @@ const AboutMe = ({ aboutme }: AboutMeData) => {
                   <h2 className={`${joseFont} fs-500`}>
                     I Also <span>Can Work</span> With
                   </h2>
-                  {skill2.map(({ tech, index }: Props) => {
+                  {skill2.map((tech: string, index: number) => {
                     return (
                       <p className={`${joseFont} fs-400`} key={index}>
                         {tech}
