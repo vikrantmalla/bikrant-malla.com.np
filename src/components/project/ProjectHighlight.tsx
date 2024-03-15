e5a89a58d6b34511725c0b609370545674086570
Vikrant Malla <vikrantmalla999@gmail.com>
2024-03-15 21:40:48 +0545

    fix: projecthiglights props types

--- src/components/project/ProjectHighlight.tsx
+++ src/components/project/ProjectHighlight.tsx
@@ -1,22 +1,18 @@
 "use client";
 import Link from "next/link";
-import { ProjectHighlightPageData, ProjectHighlights } from "../../types/data";
+import { Project, ProjectHighlightData } from "../../types/data";
 import ProjectCard from "./ProjectCard";
 import { joseFont } from "@/helpers/lib/font";
 
-const ProjectHighlight = ({
-  projectHighlightData,
-}: ProjectHighlightPageData) => {
+const ProjectHighlight = ({ project }: ProjectHighlightData) => {
   return (
     <>
       <section className="project" id="project">
         <h2 className={`headingleft ${joseFont} fs-600`}>Projects</h2>
         <div className="projectItem">
-          {projectHighlightData.projectHighlights.map(
-            (project: ProjectHighlights, index: number) => {
-              return <ProjectCard key={index} project={project} />;
-            }
-          )}
+          {project.map((project: Project, index: number) => {
+            return <ProjectCard key={index} project={project} />;
+          })}
         </div>
         <Link href="/archive" passHref aria-label="View Full Project Archive">
           <h1 className={`link ${joseFont} fs-400`}>View the Archive</h1>
