4c1de2521b6622ed05d7192a448fe60730ceb60b
Vikrant Malla <vikrantmalla999@gmail.com>
2024-03-15 23:28:10 +0545

    fix: contact props types

--- src/components/shared/footer/Contact.tsx
+++ src/components/shared/footer/Contact.tsx
@@ -1,30 +1,39 @@
 "use client";
 import { joseFont, tekoFont } from "@/helpers/lib/font";
-import { ContactPageData } from "../../../types/data";
+import { Contact, ContactInfo } from "../../../types/data";
 import SocialMedia from "./SocialMedia";
 import ExternalLink from "../externalLink";
 
-const Contact = ({ contactData }: ContactPageData) => {
+const Contact = ({ contact }: Contact) => {
   return (
     <>
       <section className="contact" id="contact">
         <h2 className={`heading ${tekoFont} fs-700`}>Get In Touch</h2>
-        {contactData?.contact.map((details, index) => (
-          <div key={index}>
-            <p className={`${joseFont} fs-400`}>{details.message}</p>
-            <ExternalLink
-              className={`button ${joseFont} fs-500`}
-              href={details.emailUrl}
-              label={`Email (opens in a new tab)`}
-              gtagAction={`email_clicked`}
-              gtagCategory="contact_interaction"
-              gtagLabel="click_through_link"
-            >
-              {details.ctaMessage}
-            </ExternalLink>
-          </div>
-        ))}
-        <SocialMedia contactData={contactData} visibleCount={4} />
+        {contact.map((details: ContactInfo, index: number) => {
+          const { message, emailUrl, ctaMessage } = details;
+          return (
+            <div key={index}>
+              <p className={`${joseFont} fs-400`}>{message}</p>
+              <ExternalLink
+                className={`button ${joseFont} fs-500`}
+                href={emailUrl}
+                label={`Email (opens in a new tab)`}
+                gtagAction={`email_clicked`}
+                gtagCategory="contact_interaction"
+                gtagLabel="click_through_link"
+              >
+                {ctaMessage}
+              </ExternalLink>
+            </div>
+          );
+        })}
+        {contact.map((details: ContactInfo, index: number) => {
+          return (
+            <div key={index}>
+              <SocialMedia {...details} visibleCount={4} />
+            </div>
+          );
+        })}
       </section>
     </>
   );
