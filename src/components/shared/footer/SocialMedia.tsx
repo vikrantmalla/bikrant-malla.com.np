b06d760f52dadef3d8c8f07f4faf68a43b782758
Vikrant Malla <vikrantmalla999@gmail.com>
2024-03-15 23:28:10 +0545

    fix: contact props types

--- src/components/shared/footer/SocialMedia.tsx
+++ src/components/shared/footer/SocialMedia.tsx
@@ -1,34 +1,35 @@
 import React from "react";
 import { FaGithub, FaBehance, FaLinkedinIn, FaTwitter } from "react-icons/fa";
-import { ContactData } from "../../../types/data";
+import { SocialMedia } from "../../../types/data";
 import ExternalLink from "../externalLink";
 
-interface SocialMediaProps {
-  contactData?: ContactData;
-  visibleCount: number;
-}
-
-const SocialMedia = ({ contactData, visibleCount }: SocialMediaProps) => {
+const SocialMedia = ({
+  githubUrl,
+  behanceUrl,
+  linkedinUrl,
+  twitterUrl,
+  visibleCount,
+}: SocialMedia) => {
   const socialMediaIcons = [
     {
       key: "github",
       icon: <FaGithub size={20} />,
-      url: contactData?.contact[0].githubUrl ?? "",
+      url: githubUrl ?? "",
     },
     {
       key: "linkedin",
       icon: <FaLinkedinIn size={20} />,
-      url: contactData?.contact[0].linkedinUrl ?? "",
+      url: linkedinUrl ?? "",
     },
     {
       key: "behance",
       icon: <FaBehance size={20} />,
-      url: contactData?.contact[0].behanceUrl ?? "",
+      url: behanceUrl ?? "",
     },
     {
       key: "twitter",
       icon: <FaTwitter size={20} />,
-      url: contactData?.contact[0].twitterUrl ?? "",
+      url: twitterUrl ?? "",
     },
   ];
 
@@ -36,20 +37,23 @@ const SocialMedia = ({ contactData, visibleCount }: SocialMediaProps) => {
   return (
     <>
       <div className="social-media">
-        {visibleSocialMediaIcons.map((socialMedia) => (
-          <ExternalLink
-            key={socialMedia.key}
-            href={socialMedia.url}
-            label={
-              `${socialMedia.key.charAt(0).toUpperCase() + socialMedia.key.slice(1)} (opens in a new tab)`
-            }
-            gtagAction={`social_media_${socialMedia.key}_clicked`}
-            gtagCategory="social_media_interaction"
-            gtagLabel="click_through_link"
-          >
-            {socialMedia.icon}
-          </ExternalLink>
-        ))}
+        {visibleSocialMediaIcons.map((socialMedia) => {
+          const { key, icon, url } = socialMedia;
+          return (
+            <ExternalLink
+              key={key}
+              href={url}
+              label={`${
+                key.charAt(0).toUpperCase() + key.slice(1)
+              } (opens in a new tab)`}
+              gtagAction={`social_media_${key}_clicked`}
+              gtagCategory="social_media_interaction"
+              gtagLabel="click_through_link"
+            >
+              {icon}
+            </ExternalLink>
+          );
+        })}
       </div>
     </>
   );
