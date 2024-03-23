08611a63a1bfae66558af3b6518687f1153a33c9
Vikrant Malla <vikrantmalla999@gmail.com>
2024-03-23 23:08:34 +0545

    feat: add a ResetPassword compoent UI in auth modal

--- src/components/adminDashboard/auth/ResetPassword.tsx
+++ src/components/adminDashboard/auth/ResetPassword.tsx
@@ -1,43 +1,46 @@
-"use client"
+"use client";
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 
 const ResetPassword = () => {
   return (
-    <form>
-      <div className="mb-4">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="email"
-        >
-          Email
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="email"
-          type="email"
-          placeholder="Enter Email"
-        />
-      </div>
-      <div className="mb-6">
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="password"
-        >
-          Password
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-          id="password"
-          type="password"
-          placeholder="Enter Password"
-        />
-      </div>
-      <div className="my-auto">
-        <button>
-          Reset Password
-        </button>
-      </div>
-    </form>
+    <Card>
+      <CardHeader>
+        <CardTitle>SignIn</CardTitle>
+      </CardHeader>
+      <form>
+        <CardContent className="space-y-2">
+          <div className="space-y-1">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" type="email" placeholder="Enter Email" />
+          </div>
+          <div className="space-y-1">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" />
+          </div>
+          <Button
+            type="button"
+            className="bg-transparent border-none text-black w-[350px]"
+          >
+            Forget Password
+          </Button>
+        </CardContent>
+        <CardFooter>
+          <Button type="submit" className="w-[350px]">
+            Reset Password
+          </Button>
+        </CardFooter>
+      </form>
+    </Card>
   );
 };
 
