5549022e16051cd0dd75dcf8ca9ff9f1e7ac3b54
Vikrant Malla <vikrantmalla999@gmail.com>
2024-03-29 22:54:10 +0545

    feat: add a types check in enviroment variable

---
+++ src/types/env.ts
@@ -0,0 +1,57 @@
+import { z } from "zod";
+
+const envVariables = z.object({
+  MONGODB_URI: z.string(),
+  NEXT_PUBLIC_APP_URL: z.string().url(),
+  NEXT_PUBLIC_DEFAULT_APP_URL: z.string().url(),
+  NEXT_PUBLIC_GOOGLE_ANALYTICS: z.string(),
+  NEXT_PUBLIC_GOOGLE_SITE_ID: z.string(),
+  NEXT_PUBLIC_MICROSOFT_CLARITY: z.string(),
+  TOKEN_SECRET: z.string(),
+  TRANSPORT_HOST: z.string(),
+  TRANSPORT_USER: z.string(),
+  TRANSPORT_PASS: z.string(),
+  MAILER: z.string().email(),
+});
+
+const {
+  MONGODB_URI,
+  NEXT_PUBLIC_APP_URL,
+  NEXT_PUBLIC_DEFAULT_APP_URL,
+  NEXT_PUBLIC_GOOGLE_ANALYTICS,
+  NEXT_PUBLIC_GOOGLE_SITE_ID,
+  NEXT_PUBLIC_MICROSOFT_CLARITY,
+  TOKEN_SECRET,
+  TRANSPORT_HOST,
+  TRANSPORT_USER,
+  TRANSPORT_PASS,
+  MAILER,
+} = process.env;
+
+const parsedResult = envVariables.safeParse({
+  MONGODB_URI,
+  NEXT_PUBLIC_APP_URL,
+  NEXT_PUBLIC_DEFAULT_APP_URL,
+  NEXT_PUBLIC_GOOGLE_ANALYTICS,
+  NEXT_PUBLIC_GOOGLE_SITE_ID,
+  NEXT_PUBLIC_MICROSOFT_CLARITY,
+  TOKEN_SECRET,
+  TRANSPORT_HOST,
+  TRANSPORT_USER,
+  TRANSPORT_PASS,
+  MAILER,
+});
+
+if (!parsedResult.success) {
+  throw new Error("There is an error with the environment variables");
+}
+
+export const enviromentVariables = parsedResult.data;
+
+type EnvVarSchemaType = z.infer<typeof envVariables>
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends EnvVarSchemaType {}
+  }
+}
