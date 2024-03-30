b42098d785d9a2a0e9bd1b32368e46057486e237
Vikrant Malla <vikrantmalla999@gmail.com>
2024-03-30 23:25:21 +0545

    feat: add a endpoint and message enum

--- src/types/enum.ts
+++ src/types/enum.ts
@@ -21,3 +21,19 @@ export enum EmailType {
   VERIFY = "VERIFY",
   RESET = "RESET",
 }
+
+// ENDPOINT
+export enum Endpoint {
+  AUTH_SIGN_UP_ENDPOINT = "/api/auth/signup",
+  AUTH_SIGN_IN_ENDPOINT = "/api/auth/signin",
+}
+
+// MESSAGE
+export enum Message {
+  USER_ALREADY_EXISTS = "User already exists",
+  USER_CREATED_SUCCESSFULLY = " User created successfully!",
+  INVALID_EMAIL = "Please enter a valid email",
+  EMAIL_REQUIRED = "Please enter your email",
+  PASSWORD_REQUIRED = "Please enter your password",
+  PASSWORD_MIN_LENGTH = "Password must be at least 4 characters long",
+}
