3a0396a88177a7abdd4f7c56d0f65659b4e5f0f9
Vikrant Malla <vikrantmalla999@gmail.com>
2024-03-30 23:23:34 +0545

    feat: URL endpoints

---
+++ src/service/endpoints.ts
@@ -0,0 +1,4 @@
+import { Endpoint } from "@/types/enum";
+
+export const AUTH_SIGN_UP_ENDPOINT = Endpoint.AUTH_SIGN_UP_ENDPOINT;
+export const AUTH_SIGN_IN_ENDPOINT = Endpoint.AUTH_SIGN_IN_ENDPOINT;
