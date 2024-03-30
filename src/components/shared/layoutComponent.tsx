370cbc1184ac215b593a879bd93ccbd0d4aa9c64
Vikrant Malla <vikrantmalla999@gmail.com>
2024-03-30 17:57:02 +0545

    feat: add a toast container component

--- src/components/shared/layoutComponent.tsx
+++ src/components/shared/layoutComponent.tsx
@@ -1,18 +1,32 @@
-"use client"
+"use client";
 import React from "react";
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 import DotRing from "./cursor/DotRing";
 import ScrollArrow from "./scrollup/ScrollArrow";
 import Footer from "./footer/Footer";
 import NavBar from "./header/NavBar";
 import CustomScript from "@/helpers/customScript/customScript";
 import { Contact } from "@/types/data";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const SharedComponent = ({contact}: Contact) => {
-    const pathname = usePathname();
-    const isNavigationEnabled = pathname !== '/admin';
+const SharedComponent = ({ contact }: Contact) => {
+  const pathname = usePathname();
+  const isNavigationEnabled = pathname !== "/admin";
   return (
     <>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
       <CustomScript />
       {isNavigationEnabled && <NavBar contact={contact} />}
       <DotRing />
