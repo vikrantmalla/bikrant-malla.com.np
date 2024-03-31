f857aaf13e1f0c296b8426154b7ae7ae178e95cd
Vikrant Malla <vikrantmalla999@gmail.com>
2024-03-31 21:20:57 +0545

    refactor: export api endpoints using enums

--- src/service/endpoints.ts
+++ src/service/endpoints.ts
@@ -1,4 +1,12 @@
-import { Endpoint } from "@/types/enum";
+import { ApiEndpoint } from "@/types/enum";
 
-export const AUTH_SIGN_UP_ENDPOINT = Endpoint.AUTH_SIGN_UP_ENDPOINT;
-export const AUTH_SIGN_IN_ENDPOINT = Endpoint.AUTH_SIGN_IN_ENDPOINT;
+export const ABOUT_ME_ENDPOINT = ApiEndpoint.ABOUT_ME_ENDPOINT;
+export const AUTH_SIGN_UP_ENDPOINT = ApiEndpoint.AUTH_SIGN_UP_ENDPOINT;
+export const AUTH_SIGN_IN_ENDPOINT = ApiEndpoint.AUTH_SIGN_IN_ENDPOINT;
+export const BEHANCE_ENDPOINT = ApiEndpoint.BEHANCE_ENDPOINT;
+export const CONTACT_ENDPOINT = ApiEndpoint.CONTACT_ENDPOINT;
+export const META_DATA_ENDPOINT = ApiEndpoint.META_DATA_ENDPOINT;
+export const PORTFOLIO_DETAILS_ENDPOINT = ApiEndpoint.PORTFOLIO_DETAILS_ENDPOINT;
+export const PROJECTS_ENDPOINT = ApiEndpoint.PROJECTS_ENDPOINT;
+export const PROJECT_HIGHLIGHTS_ENDPOINT = ApiEndpoint.PROJECT_HIGHLIGHTS_ENDPOINT;
+export const TAGS_ENDPOINT = ApiEndpoint.TAGS_ENDPOINT;
