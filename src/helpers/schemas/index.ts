9a40e9dde25445fcb9209a45286295bccb6c76f6
Vikrant Malla <vikrantmalla999@gmail.com>
2024-04-02 23:15:18 +0545

    feat: add a signin schema zod object

--- src/helpers/schemas/index.ts
+++ src/helpers/schemas/index.ts
@@ -9,3 +9,10 @@ export const signUpSchema = z.object({
     .nonempty(Message.PASSWORD_REQUIRED)
     .min(4, Message.PASSWORD_MIN_LENGTH),
 });
+
+export const signInSchema = z.object({
+  loginEmail: z.string().nonempty(Message.EMAIL_REQUIRED).email(Message.INVALID_EMAIL),
+  loginPassword: z
+    .string()
+    .nonempty(Message.PASSWORD_REQUIRED)
+});
