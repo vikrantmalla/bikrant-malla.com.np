a7a37f6e7c4ce94f1facd116a2c887d058956ca3
Vikrant Malla <vikrantmalla999@gmail.com>
2024-04-06 22:24:51 +0545

    feat: add signin, signup, resetpassword page

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignIn from "@/components/adminDashboard/auth/SignIn";
+
+const SignInPage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInPage;
