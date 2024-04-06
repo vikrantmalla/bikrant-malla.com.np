2afa10c966465b8dea99ca070248e14564f1f4d7
Vikrant Malla <vikrantmalla999@gmail.com>
2024-04-06 22:24:51 +0545

    feat: add signin, signup, resetpassword page

---
+++ src/app/resetpassword/page.tsx
@@ -0,0 +1,12 @@
+import ResetPassword from "@/components/adminDashboard/auth/ResetPassword";
+import React from "react";
+
+const ForgetPasswordPage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <ResetPassword />
+    </div>
+  );
+};
+
+export default ForgetPasswordPage;
