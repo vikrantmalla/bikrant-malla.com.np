d7f518303abf2b58700cfecd3aa66105d04099d1
Vikrant Malla <vikrantmalla999@gmail.com>
2024-04-11 22:49:06 +0545

    feat: add type safety to the behance mongoose schema

--- src/helpers/models/Behance.ts
+++ src/helpers/models/Behance.ts
@@ -1,29 +1,39 @@
-import mongoose from "mongoose";
+import { SchemaMessage } from "@/types/enum";
+import mongoose, { Schema, Document } from "mongoose";
 
-const BehanceHighlightSchema = new mongoose.Schema({
+export interface BehanceHighlight extends Document {
+  title: string;
+  subTitle: string;
+  images: string;
+  alt: string;
+  projectview: string;
+  tools: string[];
+}
+
+const BehanceHighlightSchema: Schema<BehanceHighlight> = new Schema({
   title: {
     type: String,
-    required: true,
+    required: [true, SchemaMessage.TITLE_IS_REQUIRED],
   },
   subTitle: {
     type: String,
-    required: true,
+    required: [true, SchemaMessage.SUB_TITLE_IS_REQUIRED],
   },
   images: {
     type: String,
-    required: true,
+    required: [true, SchemaMessage.IMAGE_IS_REQUIRED],
   },
   alt: {
     type: String,
-    required: true,
+    required: [true, SchemaMessage.ALT_IS_REQUIRED],
   },
   tools: {
     type: [String],
-    required: true,
+    required: [true, SchemaMessage.TOOLS_ARE_REQUIRED],
   },
   projectview: {
     type: String,
-    required: true,
+    required: [true, SchemaMessage.PROJECTVIEW_IS_REQUIRED],
   },
 });
 
