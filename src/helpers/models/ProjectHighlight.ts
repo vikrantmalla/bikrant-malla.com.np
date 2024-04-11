2b2e50866e7516317a5e17db0ef3233ce239f41e
Vikrant Malla <vikrantmalla999@gmail.com>
2024-04-11 22:49:52 +0545

    feat: add type safety to the project highlights mongoose schema

--- src/helpers/models/ProjectHighlight.ts
+++ src/helpers/models/ProjectHighlight.ts
@@ -1,25 +1,35 @@
-import mongoose from "mongoose";
+import { SchemaMessage } from "@/types/enum";
+import mongoose, { Schema, Document } from "mongoose";
 
-const ProjectHighlightSchema = new mongoose.Schema({
+export interface ProjectHighlight extends Document {
+  title: string;
+  subTitle: string;
+  images: string;
+  alt: string;
+  projectview: string;
+  build: string[];
+}
+
+const ProjectHighlightSchema: Schema<ProjectHighlight> = new Schema({
   title: {
     type: String,
-    required: true,
+    required: [true, SchemaMessage.TITLE_IS_REQUIRED],
   },
   images: {
     type: String,
-    required: true,
+    required: [true, SchemaMessage.IMAGE_IS_REQUIRED],
   },
   alt: {
     type: String,
-    required: true,
+    required: [true, SchemaMessage.ALT_IS_REQUIRED],
   },
   projectview: {
     type: String,
-    required: true,
+    required: [true, SchemaMessage.PROJECTVIEW_IS_REQUIRED],
   },
   build: {
     type: [String],
-    required: true,
+    required: [true, SchemaMessage.TOOLS_ARE_REQUIRED],
   },
 });
 
