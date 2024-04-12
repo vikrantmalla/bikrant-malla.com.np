6a59b72de87a00624080538e4454c6e68dd67bec
Vikrant Malla <vikrantmalla999@gmail.com>
2024-04-12 22:56:41 +0545

    feat: use a database url based on enviroment

--- src/helpers/lib/baseUrl.ts
+++ src/helpers/lib/baseUrl.ts
@@ -1,5 +1,9 @@
-const baseUrl =
+export const baseUrl =
   process.env.NODE_ENV === "production"
     ? process.env.NEXT_PUBLIC_APP_URL
     : process.env.NEXT_PUBLIC_DEFAULT_APP_URL;
-export default baseUrl;
+
+export const dataBaseUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.DB_PROD_MONGODB_URI
+    : process.env.DB_DEV_MONGODB_URI;
