058b980db980f531f15ef6bc7f15e340cbad0985
Vikrant Malla <vikrantmalla999@gmail.com>
2024-04-16 22:26:45 +0545

    feat: add type safety to the user mongoose schema

--- src/helpers/models/User.ts
+++ src/helpers/models/User.ts
@@ -1,35 +1,49 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 import bcrypt from "bcryptjs";
+import { SchemaMessage } from "@/types/enum";
 
-const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: [true, 'Please enter your email'],
-        unique: true,
-        trim: true,
-        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Invalid email address"]
-    },
-    password: {
-        type: String,
-        required: [true, 'Please enter your password'],
-        trim: true
-    },
-    isVerfied: {
-        type: Boolean,
-        default: false,
-    },
-    forgotPasswordToken: String,
-    forgotPasswordTokenExpiry: Date,
-    verifyToken: String,
-    verifyTokenExpiry: Date,
+export interface User extends Document {
+  email: string;
+  password: string;
+  isVerified: boolean;
+  forgotPasswordToken: string;
+  forgotPasswordTokenExpiry: number;
+  verifyToken: string;
+  verifyTokenExpiry: number;
+}
+
+const userSchema: Schema<User> = new Schema({
+  email: {
+    type: String,
+    required: [true, SchemaMessage.TITLE_IS_REQUIRED],
+    unique: true,
+    trim: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      SchemaMessage.INVALID_EMAIL,
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, SchemaMessage.PASSWORD_REQUIRED],
+    trim: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
 });
 
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
-      next();
-    }
-  
-    this.password = await bcrypt.hash(this.password, 10);
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
-export default mongoose.models.User || mongoose.model('User', userSchema)
+export default mongoose.models.User || mongoose.model("User", userSchema);
