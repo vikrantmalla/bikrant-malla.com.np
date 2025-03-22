2804f93d852be7384b39f1fbeb2b47a66216a6c8
Vikrant Malla <vikrantmalla999@gmail.com>
2025-03-22 20:16:27 +0545

    chore: remove unused code and update layoutComponent

--- src/components/shared/layoutComponent.tsx
+++ src/components/shared/layoutComponent.tsx
@@ -1,40 +1,20 @@
 "use client";
 import React from "react";
-import { usePathname } from "next/navigation";
+import { Contact } from "@/types/data";
 import DotRing from "./cursor/DotRing";
 import ScrollArrow from "./scrollup/ScrollArrow";
 import Footer from "./footer/Footer";
 import NavBar from "./header/NavBar";
 import CustomScript from "@/helpers/customScript/customScript";
-import { Contact } from "@/types/data";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 const SharedComponent = ({ contact }: Contact) => {
-  const pathname = usePathname();
-  const isNavigationEnabled =
-    pathname !== "/signin" &&
-    pathname !== "/signup" &&
-    pathname !== "/resetpassword";
   return (
     <>
-      <ToastContainer
-        position="bottom-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={true}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
       <CustomScript />
-      {isNavigationEnabled && <NavBar contact={contact} />}
+      <NavBar contact={contact} />
       <DotRing />
       <ScrollArrow />
-      {isNavigationEnabled && <Footer />}
+      <Footer />
     </>
   );
 };
