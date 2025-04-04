3d8455713633455eec92f021e4234169255eccea
Vikrant Malla <vikrantmalla999@gmail.com>
2025-04-04 23:49:57 +0545

    chore: refactor Zustand store implementation to use createStore for better organization and maintainability

--- src/store/feature/appStore.ts
+++ src/store/feature/appStore.ts
@@ -1,21 +1,17 @@
-import { create } from "zustand";
-import { devtools } from "zustand/middleware";
-import { Enviroment } from "@/types/enum";
+import { createStore } from "../provider";
 import { ProviderContext } from "@/types/data";
 
-export const useAppStore = create<ProviderContext.AppState>()(
-  devtools(
-    (set) => ({
-      toggleMenu: false,
-      navColor: false,
-      activeLink: "",
-      showForgetPasswordModal: false,
-      setToggleMenu: (value) => set({ toggleMenu: value }),
-      setNavColor: (value) => set({ navColor: value }),
-      setActiveLink: (value) => set({ activeLink: value }),
-      setShowForgetPasswordModal: (value) =>
-        set({ showForgetPasswordModal: value }),
-    }),
-    { enabled: process.env.NODE_ENV !== Enviroment.PRODUCTION, name: "app" }
-  )
+export const useAppStore = createStore<ProviderContext.AppState>(
+  (set) => ({
+    toggleMenu: false,
+    navColor: false,
+    activeLink: "",
+    showForgetPasswordModal: false,
+    setToggleMenu: (value) => set({ toggleMenu: value }),
+    setNavColor: (value) => set({ navColor: value }),
+    setActiveLink: (value) => set({ activeLink: value }),
+    setShowForgetPasswordModal: (value) =>
+      set({ showForgetPasswordModal: value }),
+  }),
+  "app"
 );
--- src/store/feature/projectStore.ts
+++ src/store/feature/projectStore.ts
@@ -1,40 +1,34 @@
-import { create } from "zustand";
-import { devtools } from "zustand/middleware";
-import { Enviroment } from "@/types/enum";
-import { ProviderContext } from "@/types/data";
+import { createStore } from "../provider";
+import { TagsCategory } from "@/types/enum";
+import { ArchiveProject, ProviderContext } from "@/types/data";
 
-export const useProjectStore = create<ProviderContext.ProjectStore>()(
-  devtools(
-    (set) => ({
-      projectList: [],
-      selectedTag: "ALL",
-      showSkeletonLoading: false,
-      isAscending: false,
+export const useProjectStore = createStore<ProviderContext.ProjectStore>(
+  (set) => ({
+    projectList: [],
+    selectedTag: TagsCategory.ALL,
+    showSkeletonLoading: false,
+    isAscending: false,
 
-      setSelectedTag: (tag) => set({ selectedTag: tag }),
+    setSelectedTag: (tag) => set({ selectedTag: tag }),
 
-      setProjectList: (projects) =>
-        set({
-          projectList: [...projects].sort(
-            (prevProject, nextProject) => nextProject.year - prevProject.year
-          ),
-        }),
+    setProjectList: (projects) =>
+      set({
+        projectList: [...projects].sort(
+          (prevProject, nextProject) => nextProject.year - prevProject.year
+        ),
+      }),
 
-      sortProjectList: () =>
-        set((state) => ({
-          isAscending: !state.isAscending,
-          projectList: [...state.projectList].sort((prevProject, nextProject) =>
-            state.isAscending
-              ? prevProject.year - nextProject.year
-              : nextProject.year - prevProject.year
-          ),
-        })),
+    sortProjectList: () =>
+      set((state: { isAscending: boolean; projectList: ArchiveProject[] }) => ({
+        isAscending: !state.isAscending,
+        projectList: [...state.projectList].sort((prevProject, nextProject) =>
+          state.isAscending
+            ? prevProject.year - nextProject.year
+            : nextProject.year - prevProject.year
+        ),
+      })),
 
-      setSkeletonLoading: (value) => set({ showSkeletonLoading: value }),
-    }),
-    {
-      enabled: process.env.NODE_ENV !== Enviroment.PRODUCTION,
-      name: "projectStore",
-    }
-  )
+    setSkeletonLoading: (value) => set({ showSkeletonLoading: value }),
+  }),
+  "projectStore"
 );
